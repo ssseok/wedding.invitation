f0df8e370057db3f57efdc2e24e9d1e6bf11ce7a
Hyun Seok Park <luckseok1@gmail.com>
2025-03-13 22:54:10 +0900

    style: share image url 변경 #14

--- src/components/banner-image.tsx
+++ src/components/banner-image.tsx
@@ -2,7 +2,7 @@ export default function BannerImage() {
   return (
     <div className='relative w-full aspect-4/5'>
       <img
-        src='https://imagedelivery.net/9PYUDgg_yiUa2u-j77sFBg/d5b8c0a2-13c7-4e81-70ee-20f263346200/tog' // 실제 이미지 URL로 변경
+        src='https://imagedelivery.net/9PYUDgg_yiUa2u-j77sFBg/d5b8c0a2-13c7-4e81-70ee-20f263346200/tog'
         alt='웨딩 배너 이미지'
         className='w-full h-full object-cover'
       />
--- src/components/share.tsx
+++ src/components/share.tsx
@@ -48,7 +48,7 @@ export default function Share() {
   const kakaoShareFeed = () => {
     kakaoSend({
       imageUrl:
-        'https://t1.daumcdn.net/brunch/service/user/d4v5/image/ZW3pLUS0Yu60Q81nwUiTdo7RWNc.jpeg',
+        'https://imagedelivery.net/9PYUDgg_yiUa2u-j77sFBg/d5b8c0a2-13c7-4e81-70ee-20f263346200/tog',
       imageWidth: 600,
       imageHeight: 450,
     });
